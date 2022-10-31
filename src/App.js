fbec63ba0191f5c163b417010c5b77dc56490b82
Pawan Shrestha <pawanshresthait@gmail.com>
2022-10-31 15:26:25 +0545

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+// import io from 'socket.io-client';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Danfe UK
+      {/* <MessengerCustomerChat pageId="117599830089578" appId="923614031382298" /> */}
     </div>
   );
 }
